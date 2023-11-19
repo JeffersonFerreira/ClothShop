8dc6fe799f5af8bf00478d402bd2708b3d36bd09
JeffJeff <jeffersonferreira.01@pm.me>
2023-11-19 10:51:42 -0300

    ✨ Added player animation

--- Assets/_GameSource/Script/Player/PlayerMovement.cs
+++ Assets/_GameSource/Script/Player/PlayerMovement.cs
@@ -1,28 +1,36 @@
 ﻿using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace Game
 {
-    public class PlayerMovement : MonoBehaviour, IDropHandler
+    public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float _moveSpeed = 5;
+        [SerializeField] private Animator _animator;
+
+        private static readonly int animIsWalking = Animator.StringToHash("isWalking");
 
         private Vector3 _movementDir;
 
         public void Move(float h, float v)
         {
-            _movementDir = new Vector3(h, v, 0);
+            _movementDir = new Vector3(h, v, 0).normalized;
+
+            // Toggle walking animation if moving
+            bool isMoving = _movementDir.sqrMagnitude > 0.01f;
+            _animator.SetBool(animIsWalking, isMoving);
+
+            // Flip character scale to make it face the same direction as the input
+            if (Mathf.Abs(h) > 0.001f)
+            {
+                var scale = Vector3.one;
+                scale.x *= Mathf.Sign(h);
+                transform.localScale = scale;
+            }
         }
 
         private void FixedUpdate()
         {
             transform.position += _movementDir * _moveSpeed * Time.deltaTime;
-        }
-
-        public void OnDrop(PointerEventData eventData)
-        {
-            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
-            Debug.Log(methodName);
         }
     }
 }
